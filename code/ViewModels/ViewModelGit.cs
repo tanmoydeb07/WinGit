6d826dde0ec989ff7cdced320daaf94fd193bb00
James Dingle <ding_jimmy@hotmail.com>
2012-04-16 13:37:57 +0100

    Added two new ViewModel classes  - ViewModelGit  - ViewModelRepo
    Signed-off-by: James Dingle <ding_jimmy@hotmail.com>

---
+++ code/ViewModels/ViewModelGit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WinGit.ViewModels
+{
+    class ViewModelGit
+    {
+        public ObservableCollection<RepoLocation> RecentlyOpened { get; set; }
+        public String RootFolder { get; set; }
+
+        public ViewModelGit()
+        {
+            RootFolder = String.Format(@"{0}\Git", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            RecentlyOpened = new ObservableCollection<RepoLocation>();
+            RecentlyOpened.Add(new RepoLocation() { Name = "RepositoryOne", FolderPath = @"~\repo1" });
+            RecentlyOpened.Add(new RepoLocation() { Name = "RepositoryTwo", FolderPath =  @"~\repo2" });
+            RecentlyOpened.Add(new RepoLocation() { Name = "RepositoryThree", FolderPath = @"~\repo3" });
+            RecentlyOpened.Add(new RepoLocation() { Name = "RepositoryFour", FolderPath = @"~\repo4" });
+        }
+
+        public void OpenRepo(string filepath)
+        {
+        }
+
+        public void CreateRepo(string filepath)
+        {
+        }
+
+        public void CloneRepo(string location)
+        {
+        }
+
+
+    }
+
+    struct RepoLocation
+    {
+        public string Name {get; set;}
+        public string FolderPath {get; set;}
+
+        public void Open()
+        {
+            
+        }
+    }
+}
