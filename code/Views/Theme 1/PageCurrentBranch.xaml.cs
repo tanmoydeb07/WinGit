2395e95cd500684f78c197703a533423725d9cb9
James Dingle <ding_jimmy@hotmail.com>
2012-04-17 21:15:29 +0100

    Modifications to Theme One
    Signed-off-by: James Dingle <ding_jimmy@hotmail.com>

---
+++ code/Views/Theme 1/PageCurrentBranch.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinGit.Views.Theme_1
+{
+    /// <summary>
+    /// Interaction logic for PageCurrentBranch.xaml
+    /// </summary>
+    public partial class PageCurrentBranch : Page
+    {
+        public PageCurrentBranch()
+        {
+            InitializeComponent();
+
+            List<string> changedFiles = new List<string>(10);
+            changedFiles.Add(@"views\window1.xaml");
+            changedFiles.Add(@"views\window1.cs");
+            changedFiles.Add(@"viewmodels\vm_main.cs");
+ 
+            DataContext = changedFiles;
+        }
+    }
+}
